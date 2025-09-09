b13c44b3160896719f8fb96ab5de02dc3abd7f30
unknown <nkansivictor@gmail.com>
2025-09-09 16:14:05 +0100

    removed the outline

--- src/Component/Searchbar.jsx
+++ src/Component/Searchbar.jsx
@@ -3,7 +3,7 @@ import { Search } from "lucide-react";
 export default function Searchbar() {
   return (
     <div className="flex p-4 gap-2 mb-4 ">
-      <input type="text" className="w-full border p-2 rounded border-gray-400"  />
+      <input type="text" className="w-full border p-2 rounded border-gray-400 outline-none "  />
       <button>
         <Search />
       </button>
