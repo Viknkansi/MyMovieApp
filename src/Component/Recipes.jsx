254564182f37698f373909ba5aa96bad49dec16b
unknown <nkansivictor@gmail.com>
2025-09-10 15:12:05 +0100

    change

--- src/Component/Recipes.jsx
+++ src/Component/Recipes.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import RecipesCard from "./RecipesCard";
 
 export default function Recipes() {
+  const [allRecipes, setAllRecipes] = useState([]);
+
+  useEffect(() => {
+    async function getAllRecipe() {
+      let res = await fetch("https://dummyjson.com/recipes");
+      if (!res.ok) {
+        throw new Error("Problem Fetching Recipes");
+      }
+
+      let data = await res.json();
+      //   console.log(data.recipes);
+      setAllRecipes(data.recipes);
+    }
+    getAllRecipe();
+  }, []);
+
   return (
     <div className="flex flex-col gap-10 ">
-      <RecipesCard />
-      <RecipesCard />
-      <RecipesCard />
+      {allRecipes.map((item, index) => (
+        <RecipesCard />
+      ))}
     </div>
   );
 }
